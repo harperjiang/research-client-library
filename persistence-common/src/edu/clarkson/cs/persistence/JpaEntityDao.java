fce990b2b2ef6bb59bed22a4df05ca54e6b97eb8
Harper Jiang <harperjiang@msn.com>
2014-07-26 21:50:55 -0400

    JpaEntityDao

--- persistence-common/src/edu/clarkson/cs/persistence/JpaEntityDao.java
+++ persistence-common/src/edu/clarkson/cs/persistence/JpaEntityDao.java
@@ -1,8 +1,22 @@
 package edu.clarkson.cs.persistence;
 
+import java.text.MessageFormat;
+
 import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
 
 public abstract class JpaEntityDao<T extends EntityObject> extends JpaDao<T> {
+
+	public T find(int id) {
+		String sql = MessageFormat.format("select t from {0} t where t.id = :id",
+				getParamClass().getSimpleName());
+		try {
+			return getEntityManager().createQuery(sql, getParamClass())
+					.setParameter("id", id).getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
 
 	public void save(T object) {
 		EntityTransaction t = getEntityManager().getTransaction();
