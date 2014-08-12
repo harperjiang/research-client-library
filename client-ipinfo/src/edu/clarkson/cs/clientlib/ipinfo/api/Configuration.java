1a84f4a621796220d81c875fd8bf142223a268a6
Harper Jiang <harperjiang@msn.com>
2014-08-12 16:17:01 -0400

    Use new key for ipinfo

--- client-ipinfo/src/edu/clarkson/cs/clientlib/ipinfo/api/Configuration.java
+++ client-ipinfo/src/edu/clarkson/cs/clientlib/ipinfo/api/Configuration.java
@@ -4,5 +4,5 @@ public class Configuration {
 
 	public static String URL = "http://api.ipaddresslabs.com/iplocation/v1.7/locateip";
 
-	public static String KEY = "SAKQF2DZ483PC7B56S9Z";
+	public static String KEY = "SAK6943HZP457BV7783Z";
 }
