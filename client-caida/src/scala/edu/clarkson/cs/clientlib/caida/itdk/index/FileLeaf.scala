c4a2ceb1a8bb9e9329b7f8a93379702d559ae787
Hao Jiang <harperjiang@msn.com>
2014-10-03 01:15:00 -0400

    Create Index

--- client-caida/src/scala/edu/clarkson/cs/clientlib/caida/itdk/index/FileLeaf.scala
+++ client-caida/src/scala/edu/clarkson/cs/clientlib/caida/itdk/index/FileLeaf.scala
@@ -5,8 +5,11 @@ import scala.collection.mutable.Buffer
 import scala.ref.SoftReference
 import java.io.FileInputStream
 import java.io.ObjectInputStream
+import java.io.FileOutputStream
+import java.io.ObjectOutputStream
+import java.io.File
 
-class FileLeaf(r: (Int, Int), fn: String) extends IndexNode(1) {
+class FileLeaf(fn: String, r: (Int, Int)) extends IndexNode(1) {
 
   var file: String = fn;
   var range = r;
@@ -18,11 +21,19 @@ class FileLeaf(r: (Int, Int), fn: String) extends IndexNode(1) {
   override def min = range._1;
   override def max = range._2;
 
-  def fetch: Unit = {
-    var ois = new ObjectInputStream(new FileInputStream(file));
+  def this(name: String, folder: String, n: IndexNode) = {
+    this(name, (n.min, n.max));
+    var oop = new ObjectOutputStream(new FileOutputStream("%s%s%s".format(folder, File.separator, name)));
+    oop writeObject node
+    oop.close
+    this.node = new SoftReference[IndexNode](n);
+  }
+
+  protected def fetch(folder: String): Unit = {
+    var ois = new ObjectInputStream(new FileInputStream("%s%s%s".format(folder, File.separator, file)));
     var filenode: IndexNode = ois.readObject().asInstanceOf[IndexNode]
-    node = new SoftReference[IndexNode](filenode);
     ois.close
+    node = new SoftReference[IndexNode](filenode);
   }
 
 }
--- client-caida/src/scala/edu/clarkson/cs/clientlib/caida/itdk/index/IndexSet.scala
+++ client-caida/src/scala/edu/clarkson/cs/clientlib/caida/itdk/index/IndexSet.scala
@@ -1,6 +1,94 @@
 package edu.clarkson.cs.clientlib.caida.itdk.index
 
-class IndexSet {
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.ObjectOutputStream
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.Buffer
+import scala.io.Source
+import org.apache.commons.io.input.CountingInputStream
+import edu.clarkson.cs.clientlib.caida.itdk.model.NodeLink
+import edu.clarkson.cs.clientlib.caida.itdk.parser.Parser
+import java.io.ObjectInputStream
 
-  var folder: String = "";
+class IndexSet(fld: String) {
+
+  var folder: String = fld;
+  if (!new File(folder).isDirectory()) throw new IllegalArgumentException();
+
+  private var root: IndexNode = null;
+  private val maxLevel = 10;
+  private val buffer = new ArrayBuffer[Buffer[IndexNode]](maxLevel);
+  private val degree = 100;
+  private val fileLevel = 4;
+  private var leafCounter = 0;
+  private var level = 0;
+
+  private val rootFile = "root"
+  private val leafFile = "leaf";
+
+  def load(input: String) = {
+    var ois = new ObjectInputStream(new FileInputStream("%s%s%s".format(folder, File.separator, rootFile)));
+    root = ois.readObject.asInstanceOf[IndexNode]
+    ois.close
+  }
+
+  def build(input: String) = {
+    // Initialize buffer structure
+    for (i <- 1 to 5) buffer += new ArrayBuffer[IndexNode](degree);
+
+    val cis = new CountingInputStream(new FileInputStream(input));
+    val parser = new Parser();
+
+    var previousNode = -1;
+    var oldoffset = cis.getByteCount();
+    var currentLeaf = new OffsetLeaf(degree);
+
+    for (line <- Source.fromInputStream(cis).getLines) {
+      var nl: NodeLink = parser.parse(line);
+      if (nl.node != previousNode) {
+        // Check current leaf and append
+        if (currentLeaf.size == degree) {
+          buffer(0) += currentLeaf;
+          merge(0, (size, lvl) => size >= degree)
+          currentLeaf = new OffsetLeaf(degree);
+        }
+        currentLeaf.append((nl.node, oldoffset));
+        previousNode = nl.node;
+      }
+      oldoffset = cis.getByteCount();
+    }
+
+    // Final cleanup and write root
+    var curlevel = level;
+    var oop = new ObjectOutputStream(new FileOutputStream(rootFile));
+    merge(0, (size, lvl) => size >= 1 && lvl <= level)
+    if (buffer(level).size > 1) {
+      merge(level, (size, lvl) => lvl == level);
+    }
+    root = buffer(level)(0);
+    oop writeObject root
+    oop.close
+  }
+
+  private def merge(i: Int, workon: (Int, Int) => Boolean): Unit = {
+    if (level < i) level = i;
+    if (workon(buffer(i).size, i)) {
+      var node = new IndexNode(buffer(i).size);
+      node buildon buffer(i)
+      buffer(i).clear
+      buffer(i + 1) += wrap(node, i + 1)
+      merge(i + 1, workon)
+    }
+  }
+
+  private def wrap(node: IndexNode, level: Int): IndexNode = {
+    if (level == fileLevel) {
+      var currentLeaf = leafCounter;
+      leafCounter += 1;
+      new FileLeaf("%s_%d".format(leafFile, currentLeaf), folder, node)
+    }
+    node
+  }
 }
