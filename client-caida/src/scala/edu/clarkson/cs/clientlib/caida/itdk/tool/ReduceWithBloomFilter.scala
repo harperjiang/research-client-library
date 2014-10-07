b563a10eea0e11b95876ef84bf1236add4fec607
Hao Jiang <harperjiang@msn.com>
2014-10-07 14:05:59 -0400

    Bloom Filter

---
+++ client-caida/src/scala/edu/clarkson/cs/clientlib/caida/itdk/tool/ReduceWithBloomFilter.scala
@@ -0,0 +1,14 @@
+package edu.clarkson.cs.clientlib.caida.itdk.tool
+
+import com.google.common.hash.BloomFilter
+import com.google.common.hash.Funnels
+
+object ReduceWithBloomFilter extends App {
+
+  // If a link had been merged once, do not merge it to other clusters.
+  // We determine this by a bloom filter
+
+  var bloomFilter =
+    BloomFilter.create[Integer](Funnels.integerFunnel(), 54000000, 0.001);
+  
+}
