4f8aeb00c3305e36017cf4534f751e00bd4074c4
Hao Jiang <harperjiang@msn.com>
2014-10-08 00:53:13 -0400

    Simple MapReduce

---
+++ client-caida/src/scala/edu/clarkson/cs/clientlib/caida/itdk/mapreduce/Sorting.scala
@@ -0,0 +1,105 @@
+package edu.clarkson.cs.clientlib.caida.itdk.mapreduce
+
+import scala.io.Source
+import java.io.FileOutputStream
+import java.io.PrintWriter
+import java.io.PrintWriter
+import java_cup.parser
+import edu.clarkson.cs.clientlib.caida.itdk.model.NodeLink
+import java.io.File
+import scala.io.Source
+import edu.clarkson.cs.clientlib.lang.HeapSorter
+import scala.sys.process.stringToProcess
+
+object Sorting {
+
+  def sort(fileName: String, output: String)(comparator: (String, String) => Int): Unit = {
+    val f = new File(fileName);
+    val line = ("cat " + fileName) #| ("wc -l") !!
+    val level = 5
+    val split = Math.pow(2, level).toInt;
+    val fileLine = Integer.parseInt(line trim) / split;
+    val smallFiles = new Array[File](split);
+    val smallSorted = new Array[File](split);
+
+    var lines =
+      for (line <- Source.fromFile(f).getLines) yield line;
+
+    var count = 0;
+    for (i <- 0 to split - 1) {
+      smallFiles(i) = new File(fileName + ".p" + i);
+      smallSorted(i) = new File(fileName + ".s0_" + i);
+      // Fill in the file
+      var pw = new PrintWriter(smallFiles(i));
+
+      if (i == split - 1) {
+        lines foreach (pw println _)
+      } else {
+        for (k <- 0 to fileLine - 1)
+          pw println lines.next
+      }
+      pw.close
+
+      heapSort(smallFiles(i), smallSorted(i), comparator)
+    }
+    for (round <- 0 to level - 1)
+      for (fi <- 0 to Math.pow(2, level - round).intValue - 1 by 2)
+        mergeSort(fileName + ".s" + round + "_" + fi,
+          fileName + ".s" + round + "_" + (fi + 1),
+          fileName + ".s" + (round + 1) + "_" + (fi / 2), comparator);
+    // Rename the merged file
+    "mv %s.s%d_%d %s".format(fileName, level, 0, output) !;
+    // Delete intermediate files
+    "rm %s.s*".format(fileName) !;
+    "rm %s.p*".format(fileName) !;
+  }
+
+  private def mergeSort(fileA: String, fileB: String, output: String, comparator: (String, String) => Int) {
+    var aline = Source.fromFile(fileA).getLines;
+    var bline = Source.fromFile(fileB).getLines;
+
+    var pw = new PrintWriter(new File(output));
+
+    var a = None: Option[String];
+    var b = None: Option[String];
+
+    while (aline.hasNext && bline.hasNext) {
+      if (a isEmpty) a = Some(aline.next);
+      if (b isEmpty) b = Some(bline.next);
+      comparator(a get, b get) match {
+        case x if x <= 0 => {
+          pw println (a.get)
+          a = None;
+        }
+        case x if x >= 0 => {
+          pw println (b.get)
+          b = None;
+        }
+        case _ => {}
+      }
+    }
+    if (!(a isEmpty)) {
+      pw println (a get)
+    }
+    if (!(b isEmpty)) {
+      pw println (b get)
+    }
+    if (aline.hasNext) {
+      aline foreach (pw println _);
+    } else {
+      bline foreach (pw println _);
+    }
+
+    pw.close
+  }
+
+  private def heapSort(fileName: File, output: File, comparator: (String, String) => Int): Unit = {
+    var dataList = Source.fromFile(fileName).getLines.toList
+
+    var sortedList = new HeapSorter().sort(dataList, comparator);
+    var pw = new PrintWriter(output);
+    sortedList foreach (pw.println(_))
+    pw close
+  }
+
+}
---
+++ client-caida/src/scala/edu/clarkson/cs/clientlib/caida/itdk/mapreduce/Task.scala
@@ -0,0 +1,41 @@
+package edu.clarkson.cs.clientlib.caida.itdk.mapreduce
+
+import java.io.FileOutputStream
+import java.io.PrintWriter
+
+import scala.io.Source
+
+object Task {
+
+  def execute(files: List[String], result: String,
+    map: (String, String) => (String, String),
+    reduce: String => String) = {
+
+    var mapname = "/tmp/%l.map".format(System currentTimeMillis)
+    var sortname = "/tmp/%l.sort".format(System currentTimeMillis)
+    var output = new PrintWriter(new FileOutputStream(mapname));
+
+    // Map
+    for (file <- files) {
+      Source.fromFile(file).getLines foreach (line => {
+        var result = map(file, line)
+        output.println("%s %s".format(result._1, result._2))
+      })
+    }
+    output.close
+
+    // Sort
+    Sorting.sort(mapname, sortname)((line1, line2) => {
+      var key1 = line1.splitAt(line1.indexOf(" "))._1
+      var key2 = line2.splitAt(line2.indexOf(" "))._1
+      key1 compare key2
+    })
+    
+    // Reduce
+    var finalout = new PrintWriter(new FileOutputStream(result));
+    Source.fromFile(sortname).getLines.map(line => line.splitAt(line.indexOf(" "))._2)
+      .map(reduce).foreach(finalout.println(_));
+    finalout.close();
+  }
+}
+
