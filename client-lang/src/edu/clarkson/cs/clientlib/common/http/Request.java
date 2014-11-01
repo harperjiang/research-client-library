2cf13c7305c259ca02f6b56d66c1c558f33fd85e
Hao Jiang <harperjiang@msn.com>
2014-11-01 10:42:35 -0400

    Move httpjson to lang

---
+++ client-lang/src/edu/clarkson/cs/clientlib/common/http/Request.java
@@ -0,0 +1,92 @@
+package edu.clarkson.cs.clientlib.common.http;
+
+import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+
+public abstract class Request<T extends Response<?>> {
+
+	protected Logger logger = LoggerFactory.getLogger(getClass());
+
+	private String url;
+
+	private HttpClient httpClient;
+
+	private Gson gson;
+
+	private JsonParser parser;
+
+	public Request(String url) {
+		super();
+		this.url = url;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public T execute() throws IOException {
+		HttpUriRequest request = buildRequest();
+		try {
+			HttpResponse response = getHttpClient().execute(request);
+			return buildResponse(response);
+		} catch (ClientProtocolException e) {
+			logger.error("Exception should be corrected", e);
+			return null;
+		}
+	}
+
+	public HttpClient getHttpClient() {
+		return httpClient;
+	}
+
+	public void setHttpClient(HttpClient httpClient) {
+		this.httpClient = httpClient;
+	}
+
+	public Gson getGson() {
+		return gson;
+	}
+
+	public void setGson(Gson gson) {
+		this.gson = gson;
+	}
+
+	public JsonParser getParser() {
+		return parser;
+	}
+
+	public void setParser(JsonParser parser) {
+		this.parser = parser;
+	}
+
+	protected abstract HttpUriRequest buildRequest();
+
+	protected T buildResponse(HttpResponse response) throws IOException {
+		try {
+			T resp = getParamClass().newInstance();
+			resp.setGson(getGson());
+			resp.setParser(getParser());
+			resp.process(response);
+
+			return resp;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	protected Class<T> getParamClass() {
+		return (Class<T>) (((ParameterizedType) getClass()
+				.getGenericSuperclass()).getActualTypeArguments()[0]);
+	}
+}
---
+++ client-lang/src/edu/clarkson/cs/clientlib/common/http/Response.java
@@ -0,0 +1,104 @@
+package edu.clarkson.cs.clientlib.common.http;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.MessageFormat;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+public abstract class Response<T extends Object> {
+
+	protected Logger logger = LoggerFactory.getLogger(getClass());
+
+	protected ResponseError error;
+
+	private JsonParser parser;
+
+	private Gson gson;
+
+	protected Response() {
+		super();
+	}
+
+	protected void process(HttpResponse response) throws IOException {
+		this.statusCode = response.getStatusLine().getStatusCode();
+
+		if (statusCode >= HttpStatus.SC_OK
+				&& statusCode < HttpStatus.SC_MULTIPLE_CHOICES) {
+			result = buildResult(response.getEntity().getContent());
+		} else {
+			logger.warn(MessageFormat.format("{0} Http Exception", statusCode));
+			result = null;
+			try {
+				error = buildError(statusCode, response.getEntity()
+						.getContent());
+			} catch (Exception e) {
+				error = new ResponseError();
+				error.setCode(statusCode);
+			}
+		}
+	}
+
+	public JsonParser getParser() {
+		return parser;
+	}
+
+	public void setParser(JsonParser parser) {
+		this.parser = parser;
+	}
+
+	public Gson getGson() {
+		return gson;
+	}
+
+	public void setGson(Gson gson) {
+		this.gson = gson;
+	}
+
+	protected abstract ResponseError buildError(int errorCode,
+			InputStream content);
+
+	protected T buildResult(InputStream content) {
+		JsonElement json = getParser().parse(new InputStreamReader(content));
+		return buildResult(json);
+	}
+
+	public static boolean isNull(JsonElement element) {
+		return element == null || element.isJsonNull();
+	}
+
+	protected abstract T buildResult(JsonElement json);
+
+	protected int statusCode;
+
+	protected T result;
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public T getResult() {
+		return result;
+	}
+
+	public ResponseError getError() {
+		return error;
+	}
+
+	public void setError(ResponseError error) {
+		this.error = error;
+	}
+
+}
